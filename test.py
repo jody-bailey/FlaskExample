e91bfd8c3b5befc45720aedd10317736ed459643
Jody Bailey <jodybailey93@gmail.com>
2020-12-01 14:25:40 -0500

    Added tests

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+import app
+
+class TestExample(unittest.TestCase):
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+    
+    def test_example(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+
+if __name__ == '__main__':
+    unittest.main()
